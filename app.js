32c28ba3206b45067a68fbede345b91414bd6a37
Luis Miguel <lmcolumna@ssp.df.gob.mx>
2019-02-06 14:04:53 -0600

    segundo commit

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log("Base de datos  : \x1b[32m%s\x1b[0m", ' online');
 });
 
-
 //Rutas
-
 app.get('/', (req, res, next) => {
 
     res.status(200).json({
