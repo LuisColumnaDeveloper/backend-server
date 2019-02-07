4c587d3eddd3927169a0807eb7749a010cfae6c6
LuisColumnaDeveloper <lmcolumna@gmail.com>
2019-02-06 22:07:31 -0600

    Crear usuario POST backend node

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 //Requires
 var express = require('express');
-// getting-started.js
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //inicializar variables
 var app = express();
+
+
+
+//Body parser
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
 
 
 //Conexión a la BD
@@ -16,14 +32,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 //Rutas
-app.get('/', (req, res, next) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
 
-});
 
 //Escuchar peticiones
 app.listen(3000, () => {
