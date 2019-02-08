13178b1f0de3318f43fb987340dc2f4aa544130f
LuisColumnaDeveloper <lmcolumna@gmail.com>
2019-02-07 20:53:35 -0600

    CRUD usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,6 @@
 //Requires
 var express = require('express');
-//inicializar variables
+var bcrypt = require('bcryptjs');
 var app = express();
 
 var Usuario = require('../models/usuario');
@@ -32,6 +32,61 @@ app.get('/', (req, res, next) => {
 });
 
 // ================================
+// Actualizar un nuevo usuario
+// ================================
+
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar Usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'el Usuario con el id: ' + id + 'no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+
+
+        usuario.save((err, usuarioGuardado) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar Usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            })
+
+        })
+    })
+
+})
+
+// ================================
 // Crear un nuevo usuario
 // ================================
 
@@ -41,7 +96,7 @@ app.post('/', (req, res) => {
     var usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
@@ -49,7 +104,7 @@ app.post('/', (req, res) => {
     usuario.save((err, usuarioGuardado) => {
 
         if (err) {
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 mensaje: 'Error al crear Usuario',
                 errors: err
@@ -62,6 +117,40 @@ app.post('/', (req, res) => {
         })
     })
 
+});
+
+
+// ================================
+// Eliminar un usuario por el ID
+// ================================
+
+app.delete('/:id', (req, res) => {
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar Usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No exite un Usuario con ese ID',
+                errors: { message: 'No exite un Usuario con ese ID' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
 })
 
 module.exports = app;
