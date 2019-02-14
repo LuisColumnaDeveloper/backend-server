61776854eec36553f3079068ae2596655296a939
Luis Miguel <lmcolumna@ssp.df.gob.mx>
2019-02-14 15:34:59 -0600

    google sign-in

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@este-es@-unseed-dificil';
+
+//Google
+module.exports.CLIENT_ID ='968544544135-4hq9fcpsolff03j458atom51upo57am6.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -8,8 +8,67 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+//Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
 // ================================
-// Obtener todos los usuarios
+// Autenticación de Google
+// ================================
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  
+  const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+
+      return{
+
+        // nombre:payload.name,
+        // email:payload.email,
+        // img:payload.picture,
+        google:true,
+        
+      }
+}
+
+
+app.post('/google', async(req,res) =>{
+
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch( e => {
+
+            return res.status(403).json({
+                ok:false,
+                mensaje:'token no valido'
+            });
+    });
+
+
+    return res.status(200).json({
+            ok: true,
+            mesnaje:'OK!!',
+            googleUser:googleUser
+        })
+
+
+});
+
+
+
+// ================================
+// Autenticación normal
 // ================================
 
 app.post('/', (req, res) => {
